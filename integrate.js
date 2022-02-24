41b298fb105194327029a62ba0b8f48a1a963969
Omar Juma <jumao2669@gmail.com>
2022-02-24 08:55:15 +0300

    added the bootstrap link and src files

---
+++ integrate.js
@@ -0,0 +1,4 @@
+if (!$.fn.modal) {
+    document.write('<script src="bootstrap-4.1.3-dist/js/bootstrap.min.js"> </script > ');
+    document.write(' <link rel ="stylesheet" href="bootstrap-4.1.3-dist/css/bootstrap.min.css"');
+}
