5ff576b4ccc543eb7239b9942b053fd5feeb507c
Omar Juma <jumao2669@gmail.com>
2022-02-24 09:01:32 +0300

    test bootstrap if working

--- integrate.js
+++ integrate.js
@@ -1,4 +1,5 @@
 if (!$.fn.modal) {
     document.write('<script src="bootstrap-4.1.3-dist/js/bootstrap.min.js"> </script > ');
     document.write(' <link rel ="stylesheet" href="bootstrap-4.1.3-dist/css/bootstrap.min.css"');
+    alert("bootstrap looded successfully");
 }
