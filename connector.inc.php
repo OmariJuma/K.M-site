679bf47a283b5e49e51a7081bbb22bd88858a6a7
Omar Juma <jumao2669@gmail.com>
2022-03-11 18:50:07 +0300

    Backend-should be working

--- connector.inc.php
+++ connector.inc.php
@@ -1,28 +1,38 @@
 
 <?php
 $server="localhost";
-$user="Kennedy2_Public";
-$pwd="gitHub5400";
-$dbase="kennedy2_Response"; 
+$user="Kennedy2_Public";//
+$pwd="gitHub5400";//
+$dbase="kennedy2_Response";//
 
 
-$con=mysqli_connect($server,$user,$pwd,$dbase);
-if(!$con){
-echo "Database not connected";
-header("location:index.html?database not connected");
-}
+$conn=mysqli_connect($server,$user,$pwd,$dbase);
+
 if(isset($_POST['submit'])){
-    if(!empty($_POST['FirstName']) && !empty($_POST['SecondName']) && !empty($_POST['Email']) && !empty($_POST['Message'])){
+    if(!empty($_POST['FirstName']) && !empty($_POST['SecondName']) && !empty($_POST['Email']) && !empty($_POST['Comment']))
+    {
         
-        $FirstName=mysqli_real_escape_string($con,$_POST['FirstName']);
-        $SecondName=mysqli_real_escape_string($con,$_POST['SecondName']);
-        $Email=mysqli_real_escape_string($con,$_POST['Email']);
-        $Message=mysqli_real_escape_string($con,$_POST['Message']);
-        $sql="INSERT INTO GeneralComments(FirstName,SecondName,Email, `Message`) VALUES(`$FirstName`,`$SecondName`,`$Email`,`$Message`);";
-        
-        header("location:index.html?feedback=success");}
+        $FirstName=mysqli_real_escape_string($conn,$_POST['FirstName']);
+        $SecondName=mysqli_real_escape_string($conn,$_POST['SecondName']);
+        $Email=mysqli_real_escape_string($conn,$_POST['Email']);
+        $Comment=mysqli_real_escape_string($conn,$_POST['Comment']);
+        $sql="insert into kennedy2_generalcomments(FirstName,SecondName, Email, Comment) values ('$FirstName','$SecondName','$Email','$Comment');";
+        $run=mysqli_query($conn,$sql);
+        if($run==true)
+        {
+            echo "Form submitted successfully";
+            header("location:index.html?feedback=success");
+           
+        }
+        else{
+         
+            echo "Form not submitted".mysqli_error($conn);
+           
+        }
+    }
 
-} else{
+} 
+else{
     echo "All fields required";
 }
  
