ff4ad7e380697a65962967d72d7dd4e2e7aac16c
Omar Juma <jumao2669@gmail.com>
2022-03-12 10:14:44 +0300

    updated the meet section

--- connector.inc.php
+++ connector.inc.php
@@ -1,22 +1,22 @@
 
 <?php
 $server="localhost";
-$user="Kennedy2_Public";//
-$pwd="gitHub5400";//
-$dbase="kennedy2_Response";//
+$user="Kennedy2_Public";
+$pwd="gitHub5400";
+$dbase="kennedy2_Response";
 
 
 $conn=mysqli_connect($server,$user,$pwd,$dbase);
 
 if(isset($_POST['submit'])){
-    if(!empty($_POST['FirstName']) && !empty($_POST['SecondName']) && !empty($_POST['Email']) && !empty($_POST['Comment']))
+    if(!empty($_POST['firstname']) && !empty($_POST['secondname']) && !empty($_POST['email']) && !empty($_POST['comm']))
     {
         
-        $FirstName=mysqli_real_escape_string($conn,$_POST['FirstName']);
-        $SecondName=mysqli_real_escape_string($conn,$_POST['SecondName']);
-        $Email=mysqli_real_escape_string($conn,$_POST['Email']);
-        $Comment=mysqli_real_escape_string($conn,$_POST['Comment']);
-        $sql="insert into kennedy2_generalcomments(FirstName,SecondName, Email, Comment) values ('$FirstName','$SecondName','$Email','$Comment');";
+        $firstname=mysqli_real_escape_string($conn,$_POST['firstname']);
+        $secondname=mysqli_real_escape_string($conn,$_POST['secondname']);
+        $email=mysqli_real_escape_string($conn,$_POST['email']);
+        $comm=mysqli_real_escape_string($conn,$_POST['comm']);
+        $sql="insert into kennedy2_GeneralComments(firstname, secondname, email, comm) values ('$firstname','$secondname','$email','$comm');";
         $run=mysqli_query($conn,$sql);
         if($run==true)
         {
